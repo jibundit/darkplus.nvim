72d6fe493482e447d2bf731e353703b191becd59
Bundit Jianpinitnan <bunditj@gmail.com>
2025-06-30 14:14:57 +0700

    Change Cursor color

--- lua/darkplus/theme.lua
+++ lua/darkplus/theme.lua
@@ -293,7 +293,7 @@ theme.set_highlights = function()
   hl(0, "MatchWordCur", { fg = 'NONE', bg = c.reference })
 
   -- hl(0, "MatchParen", { fg = c.ui_white, bg = c.red3, bold = true })
-  -- hl(0, "MatchParen", { fg = c.red, bg = c.red3, bold = true })
+  hl(0, "MatchParen", { fg = c.red, bg = c.red3, bold = true })
   hl(0, "MatchParenCur", { fg = c.alt_fg, bg = c.red3, bold = true })
 
   -- IndentBlankline
